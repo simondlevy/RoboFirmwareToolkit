a2b8dcbb5548b3aad81b884f6f748b211770bae1
simondlevy <simon.d.levy@gmail.com>
2021-03-17 01:22:19 -0400

    Cleanup

--- extras/parser/python/msppg/__init__.py
+++ extras/parser/python/msppg/__init__.py
@@ -3,13 +3,12 @@ Python implementation of MSPPG parser
 
 Auto-generated code: DO NOT EDIT!
 
-Copyright (C) Rob Jones, Alec Singer, Chris Lavin, Blake Liebling, Simon D. Levy 2021
+Copyright (C) Rob Jones, Alec Singer, Chris Lavin, Blake Liebling,
+Simon D. Levy 2021
 
 MIT License
 '''
 
-import struct
-import sys
 
 class Parser(object):
 
@@ -19,38 +18,39 @@ class Parser(object):
 
     def parse(self, char):
         '''
-        Parses one character, triggering pre-set handlers upon a successful parse.
+        Parses one character, triggering pre-set handlers upon a successful
+        parse.
         '''
 
         byte = ord(char)
 
-        if self.state ==  0: # sync char 1
-            if byte == 36: # $
+        if self.state == 0:  # sync char 1
+            if byte == 36:  # $
                 self.state += 1
 
-        elif self.state ==  1: # sync char 2
-            if byte == 77: # M
+        elif self.state == 1:  # sync char 2
+            if byte == 77:  # M
                 self.state += 1
-            else: # restart and try again
+            else:  # restart and try again
                 self.state = 0
 
-        elif self.state ==  2: # direction
+        elif self.state == 2:  # direction
             if byte == 62:  # >
                 self.message_direction = 1
-            else: # <
+            else:  # <
                 self.message_direction = 0
             self.state += 1
-            
-        elif self.state ==  3:
+
+        elif self.state == 3:
             self.message_length_expected = byte
             self.message_checksum = byte
             # setup arraybuffer
             self.message_buffer = b''
             self.state += 1
 
-        elif self.state ==  4:
+        elif self.state == 4:
             self.message_id = byte
-            self.message_length_received  = 0
+            self.message_length_received = 0
             self.message_checksum ^= byte
             if self.message_length_expected > 0:
                 # process payload
@@ -59,14 +59,14 @@ class Parser(object):
                 # no payload
                 self.state += 2
 
-        elif self.state ==  5: # payload
+        elif self.state == 5:  # payload
             self.message_buffer += char
             self.message_checksum ^= byte
             self.message_length_received += 1
             if self.message_length_received >= self.message_length_expected:
                 self.state += 1
 
-        elif self.state ==  6:
+        elif self.state == 6:
             if self.message_checksum == byte:
                 # message received, process
 
@@ -80,16 +80,14 @@ class Parser(object):
 
         else:
             print('Unknown state detected: %d' % self.state)
-            
+
     @staticmethod
     def crc8(data):
 
         crc = 0x00
-       
+
         for c in data:
 
-            crc ^= ord(c) if sys.version[0] == '2' else c
+            crc ^= c
 
         return crc
-
-
